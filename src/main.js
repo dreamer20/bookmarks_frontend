e20fb87117eb26a36e8568162309336b42e6a211
dreamer20 <gv4alex@gmail.com>
2022-12-28 17:09:04 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createVuestic } from 'vuestic-ui'
+
+import App from './App.vue'
+import router from './router'
+
+import 'vuestic-ui/css'
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(createVuestic())
+app.use(router)
+
+app.mount('#app')
